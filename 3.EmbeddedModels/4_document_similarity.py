04aa8c130c11794470890754d6c6f9d4847f3007
Kara <kara_himanshu@yahoo.com>
2025-02-18 21:35:13 +0530

    Document Similarity

---
+++ 3.EmbeddedModels/4_document_similarity.py
@@ -0,0 +1,21 @@
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings(model='text-embedding-3-large', dimensions=300)
+
+documents = [
+    "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
+    "MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
+    "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
+    "Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
+    "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
+]
+
+query = "Tell me about Virat Kohli"
+
+doc_embedding = OpenAIEmbeddings()
+
---
+++ 3.EmbeddedModels/4_document_similarity_copy.py
@@ -0,0 +1,38 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+load_dotenv()
+
+embedding = HuggingFaceEmbeddings(model_name ='sentence-transformers/all-MiniLM-L6-v2')
+
+documents = [
+    "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
+    "MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
+    "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
+    "Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
+    "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
+]
+
+query = "Who is God of Cricket?"
+
+doc_embeddings = embedding.embed_documents(documents)
+query_embedding = embedding.embed_query(query)
+
+scores = cosine_similarity([query_embedding], doc_embeddings)[0]
+
+index, score = sorted(list(enumerate(scores)),key=lambda x:x[1])[-1]
+
+print(query)
+print(documents[index])
+print("similarity score is:", score)
+
+# # print(str(doc_vector))
+# # print(str(query_vector))
+# vec1 = np.array(doc_vector)
+# vec2 = np.array(query_vector)
+
+
+# similarity = cosine_similarity(vec1, vec2)
+# print(similarity)
